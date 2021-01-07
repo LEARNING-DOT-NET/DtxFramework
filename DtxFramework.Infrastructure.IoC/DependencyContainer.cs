b9259fcb60e47f120ac6d3559c5013e6e73a2c27
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2021-01-07 15:04:19 +0330

    Add DtxFramework.Infrastructure.IoC Class Library (.NET Core 5.0)

---
+++ DtxFramework.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DtxFramework.Infrastructure.IoC
+{
+	public static class DependencyContainer
+	{
+		static DependencyContainer()
+		{
+		}
+
+		public static void RegisterServices
+			(Microsoft.Extensions.DependencyInjection.IServiceCollection services)
+		{
+			// Domain Bus
+			services.AddTransient
+				<Domain.Core.Bus.IEventBus, Bus.RabbitMQEventBus>();
+		}
+	}
+}
