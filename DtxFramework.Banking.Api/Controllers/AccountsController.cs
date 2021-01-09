fd0c4d06351cb969a64625c3ebac2c2dbc00c02e
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2021-01-09 17:14:45 +0330

    Add AccountsController

---
+++ DtxFramework.Banking.Api/Controllers/AccountsController.cs
@@ -0,0 +1,26 @@
+﻿namespace DtxFramework.Banking.Api.Controllers
+{
+	[Microsoft.AspNetCore.Mvc.ApiController]
+	[Microsoft.AspNetCore.Mvc.Route(template: "api/[controller]")]
+	public class AccountsController : Microsoft.AspNetCore.Mvc.Controller
+	{
+		public AccountsController
+			(Application.Interfaces.IAccountService accountService)
+		{
+			AccountService = accountService;
+		}
+
+		protected Application.Interfaces.IAccountService AccountService { get; }
+
+		[Microsoft.AspNetCore.Mvc.HttpGet]
+		public Microsoft.AspNetCore.Mvc.ActionResult
+			<System.Collections.Generic.IEnumerable<Domain.Models.Account>>
+			Get()
+		{
+			var result =
+				AccountService.GetAll();
+
+			return Ok(value: result);
+		}
+	}
+}
